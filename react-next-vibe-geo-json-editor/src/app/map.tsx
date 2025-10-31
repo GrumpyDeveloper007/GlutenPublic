9efafd042ada49dab82b5cbbd0dd0df498779e60
DalePitman <dale.pitman76@gmail.com>
2025-10-31 11:50:33 +1000

    Disable map wrapping and allow the use of full screen width

--- react-next-vibe-geo-json-editor/src/app/map.tsx
+++ react-next-vibe-geo-json-editor/src/app/map.tsx
@@ -695,6 +695,7 @@ export default function Map({ setSelectedTopicGroup, selectedPoints, setSelected
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    noWrap={true}
                 />
                 <DragControl />
                 <SelectionBox
